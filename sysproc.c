afe6e7cdf39c7e18530dd6d99a3c19db4524d2e5
Setare <37214449+fj1403@users.noreply.github.com>
2020-06-04 13:53:36 -0700

    add struct to sysproc.c

--- sysproc.c
+++ sysproc.c
@@ -89,8 +89,8 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
-// int
-// sys_info_proc(void)
-// {
-//   return info_proc();
-// }
+int
+sys_info_proc(void)
+{
+  return sys_info_proc();
+}
