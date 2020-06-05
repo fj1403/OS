e630854fa71cda83038496362e5ffe64449608b0
Setare <37214449+fj1403@users.noreply.github.com>
2020-06-05 11:55:49 -0700

    change test file

--- test.c
+++ test.c
@@ -10,41 +10,55 @@ struct proc_info {
 };
 
 
+
 int main(void){
-    int pid=fork();
-    if(pid==0){
+    int first_child=fork();
+    if (first_child<0)
+    {
+        printf(1,"first child failed");
+    }
+
+    
+    else if(first_child==0)
+    {
         // write a big loop to dont finish process 
-        char* t = malloc(7000000* sizeof(char*));
-        for(int i=0;i<7000000;i++)
+        char* t = malloc(50000000* sizeof(char*));
+        for(int i=0;i<50000000;i++)
         {
                         t[i]='t';
         }
-        while(0<1){
-        }
     }
-    else if(pid>0) {
+
+    else {
+        
         int second_child =fork();
-        if(second_child==0){
-            char* s=malloc(1200000* sizeof(char(*)));
-            for(int i=0;i<1200000;i++){
-                s[i]='s';
-            }
-            while(0<1){
-
-            }
+        if(second_child<0)
+        {
+            printf(1,"second_child failed");
         }
-        else if(second_child>0) {
-            sleep(20);
-            struct proc_info p [NPROC];
-            info_proc(p);
-            for(int i=0;i<NPROC;i++){
-                if(p[i].pid==0)
-                    break;
-                printf(1,"%d %d\n", p[i].memsize, p[i].pid);
-
+    else if (second_child==0)
+        {
+            char* b=malloc(7000000* sizeof(char*));
+            for (int i = 0; i < 7000000; i++)
+            {
+                b[i]='b';
             }
+            
         }
-    }
+         else{
+             struct proc_info *p=malloc(100*sizeof(struct proc_info));
+             for (int i=0;i<10;i++)
+             {
+                 if(p[i].pid!=0)
+                 {
+                 printf(1,"%d\n %d\n", p[i].memsize, p[i].pid);
+                 break;
+                }
+             }
 
-    exit();
+        }
+
+     }
+
+  //  exit();
 }
